59a1fe83087f2925ad583075538b326761c81040
dhantosy <dhantosy@gmail.com>
2015-07-21 09:24:10 +0700

    Update jQuery version, and update css

--- header.php
+++ header.php
@@ -37,10 +37,20 @@
 
     <link href='http://fonts.googleapis.com/css?family=Josefin+Slab:400,700|Roboto+Slab' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/style.css">
-<!-- 
+
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
-     -->
-    <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+    
 
   </head>
   <body class="<?php echo $bodyClass;?>">
+
+ <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58073816-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
